320d44fdcc32bdc600404607d7d15ddecf9a1c9d
MZielinski <20ZielakGitHub10>
2013-11-24 21:06:06 +0100

    Replace xml spring configuration with java class configuration

---
+++ src/main/java/com/mzielinski/citek/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.mzielinski.citek.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * @author mzielinski, Rule Financial
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.mzielinski.citek.controller")
+public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+}
